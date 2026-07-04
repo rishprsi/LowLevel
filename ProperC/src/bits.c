5dbd741b24030e11e3ceee564a54c6da03950dac
Rishabh <rishprsi@gmail.com>
2026-07-03 17:45:59 -0700

    bit

--- ProperC/src/bits.c
+++ ProperC/src/bits.c
@@ -1,40 +1,48 @@
 #include "bits.h"
-#include "todo.h"
 
 int count_set_bits(uint32_t x) {
-    (void)x;
-    TODO("implement count_set_bits");
+  int count = 0;
+  while (x) {
+    x &= x - 1;
+    count++;
+  }
+  return count;
 }
 
 bool is_power_of_two(uint32_t x) {
-    (void)x;
-    TODO("implement is_power_of_two");
+  return x != 0 && ((x & x - 1) == 0);
+  /* int count = count_set_bits((uint32_t)x); */
+  /* return (count == 1); */
 }
 
 uint32_t reverse_bits(uint32_t x) {
-    (void)x;
-    TODO("implement reverse_bits");
+  uint32_t new = 0;
+  for (int i = 0; i < 31; i++) {
+    new |= (x & 1);
+    x >>= 1;
+    new <<= 1;
+  }
+  new |= (x & 1);
+  return new;
 }
 
 uint32_t set_bit(uint32_t x, int n) {
-    (void)x;
-    (void)n;
-    TODO("implement set_bit");
+  x |= 1U << n;
+  return x;
 }
 
-uint32_t clear_bit(uint32_t x, int n) {
-    (void)x;
-    (void)n;
-    TODO("implement clear_bit");
-}
+uint32_t clear_bit(uint32_t x, int n) { return x & ~(1U << n); }
 
-uint32_t toggle_bit(uint32_t x, int n) {
-    (void)x;
-    (void)n;
-    TODO("implement toggle_bit");
-}
+uint32_t toggle_bit(uint32_t x, int n) { return x ^ (1U << n); }
 
 int count_trailing_zeros(uint32_t x) {
-    (void)x;
-    TODO("implement count_trailing_zeros");
+  if (x == 0) {
+    return 32;
+  }
+  int count = 0;
+  while (x % 2 == 0) {
+    count++;
+    x = x >> 1;
+  }
+  return count;
 }
