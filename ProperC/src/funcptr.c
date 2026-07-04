c9fecc5e61589ed9b7ec62b957c081f0409c9e27
Rishabh <rishprsi@gmail.com>
2026-07-03 22:42:57 -0700

    Properc: funcptr

--- ProperC/src/funcptr.c
+++ ProperC/src/funcptr.c
@@ -1,32 +1,38 @@
 #include "funcptr.h"
-#include "todo.h"
 
 void int_map(int *arr, size_t n, int (*fn)(int)) {
-    (void)arr;
-    (void)n;
-    (void)fn;
-    TODO("implement int_map");
+  for (size_t i = 0; i < n; i++) {
+    arr[i] = fn(arr[i]);
+  }
 }
 
 size_t int_filter(const int *src, size_t n, int *dst, bool (*pred)(int)) {
-    (void)src;
-    (void)n;
-    (void)dst;
-    (void)pred;
-    TODO("implement int_filter");
+  size_t count = 0;
+  for (size_t i = 0; i < n; i++) {
+    if (pred(src[i])) {
+      dst[count] = src[i];
+      count++;
+    }
+  }
+  return count;
 }
 
 int int_reduce(const int *arr, size_t n, int init, int (*op)(int acc, int x)) {
-    (void)arr;
-    (void)n;
-    (void)init;
-    (void)op;
-    TODO("implement int_reduce");
+  for (size_t i = 0; i < n; i++) {
+    init = op(init, arr[i]);
+  }
+
+  return init;
 }
 
 void sort_ints(int *arr, size_t n, int (*cmp)(int a, int b)) {
-    (void)arr;
-    (void)n;
-    (void)cmp;
-    TODO("implement sort_ints");
+  for (size_t i = 0; i < n - 1; i++) {
+    for (size_t j = i + 1; j < n; j++) {
+      if (cmp(arr[i], arr[j]) == 1) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
 }
